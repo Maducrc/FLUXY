5af090f6c946ddd82057fdce0b234ef0e06b22e4
Maria Eduarda Chella <164084351+Maducrc@users.noreply.github.com>
2026-08-29 18:29:00 -0300

    Tirei o Node
    Voltou a ser como estava antes

--- app.js
+++
@@ -1,6 +0,0 @@
-var app = require('./fluxy/config/express')();
-
-app.listen(3000, function() {
-    console.log("Servidor rodando na porta 3000");
-    console.log("Acesse: http://localhost:3000/login");
-});
